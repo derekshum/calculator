5cf3fbb395d63e11181170ff9213c1c560b72575
derekshum <derekshum@users.noreply.github.com>
2024-12-28 18:41:35 -0500

    + and - functionality

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple calculator app. Follow basic python running and stopping to use.
---
+++ main.py
@@ -0,0 +1,50 @@
+
+def get_number_from_digits(digit_stack: list[int]) -> int:
+    digits: int = 0
+    current_number: int = 0
+    while len(digit_stack) > 0:
+        current_number += int(digit_stack.pop()) * 10 ** digits
+        digits += 1
+    return current_number
+
+
+def calculate(s: str) -> int:
+    digit_stack: list[int] = []
+    previous_operator_positive: bool = True
+    total: int = 0
+    for c in s:
+        if c == ' ':
+            pass # 2 3 treated as 23
+        elif c.isdigit():
+            digit_stack.append(int(c))
+        elif c in ['+', '-']:
+            if len(digit_stack) > 0:
+                current_number: int = get_number_from_digits(digit_stack)
+
+                if previous_operator_positive:
+                    total += current_number
+                else:
+                    total -= current_number
+
+                if c == '+':
+                    previous_operator_positive = True
+                else:
+                    previous_operator_positive = False
+            elif c == '-':
+                previous_operator_positive = not previous_operator_positive
+
+    current_number: int = get_number_from_digits(digit_stack)
+
+    if previous_operator_positive:
+        total += current_number
+    else:
+        total -= current_number
+
+    return total
+
+
+if __name__ == '__main__':
+    while True:
+        print('Type your equation in and press enter (only +, -, and numbers will be considered currently)')
+        equation = input()
+        print(calculate(equation))
